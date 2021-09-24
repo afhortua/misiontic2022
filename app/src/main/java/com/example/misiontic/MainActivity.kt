b8b44532d92d7dc47c0e202e8906e4bad933ea4b
Andres Hortua <ahortua@unal.edu.co>
2021-09-23 21:08:27 -0500

    Edit POI detail fragment

--- app/src/main/java/com/example/misiontic/MainActivity.kt
+++ app/src/main/java/com/example/misiontic/MainActivity.kt
@@ -1,13 +1,15 @@
 package com.example.misiontic
-import android.annotation.SuppressLint
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-@SuppressLint("WrongViewCast")
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val trans = supportFragmentManager.beginTransaction()
+        val frag = POIDetail()
+        trans.replace(R.id.frag, frag)
+        trans.commit()
     }
 }
