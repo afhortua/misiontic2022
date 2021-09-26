fe1ab0702dd6931b055ee91b3fdb8e3f6a90c678
Andres Hortua <ahortua@unal.edu.co>
2021-09-26 15:15:44 -0500

    Add Settings Fragment

--- app/src/main/java/com/example/misiontic/MainActivity.kt
+++ app/src/main/java/com/example/misiontic/MainActivity.kt
@@ -2,8 +2,9 @@ package com.example.misiontic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.example.misiontic.databinding.ActivityMainBinding
-import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,9 +12,32 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.overflow, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.itemConfig -> {
+                supportFragmentManager.beginTransaction().replace(R.id.frag, SettingsFragment())
+                    .addToBackStack(null).commit()
+                return true
+            }
+            R.id.itemAbout -> {
+                supportFragmentManager.beginTransaction().replace(R.id.frag, AboutFragment())
+                    .addToBackStack(null).commit()
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 }
+
